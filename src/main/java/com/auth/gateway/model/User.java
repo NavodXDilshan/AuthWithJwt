8a163369c7f8efcf983f8762768b140f364df544
NavodXDilshan <en97569@foe.sjp.ac.lk>
2026-07-28 00:41:29 +0530

    JWT service implemented

---
+++ src/main/java/com/auth/gateway/model/User.java
@@ -0,0 +1,65 @@
+package com.auth.gateway.model;
+
+import jakarta.persistence.*;
+import jakarta.websocket.server.ServerEndpoint;
+import lombok.*;
+import org.hibernate.sql.ast.tree.expression.Over;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column
+    private boolean enabled;
+    @Column(name="verification_code")
+    private String verificationCode;
+    @Column(name="verification_expiration")
+    private LocalDateTime verficationCodeExpiresAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(){
+        return enabled;
+    }
+
+
+
+}
