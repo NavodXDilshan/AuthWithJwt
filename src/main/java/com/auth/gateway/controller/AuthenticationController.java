0d96f62666a89988d4e35736a0fe64dcd83dcb66
NavodXDilshan <en97569@foe.sjp.ac.lk>
2026-07-31 00:03:02 +0530

    Authentication controller

--- src/main/java/com/auth/gateway/controller/AuthenticationController.java
+++ src/main/java/com/auth/gateway/controller/AuthenticationController.java
@@ -1,4 +1,61 @@
 package com.auth.gateway.controller;
 
+import com.auth.gateway.dto.LoginUserDto;
+import com.auth.gateway.dto.RegisterUserDto;
+import com.auth.gateway.dto.VerifyUserDto;
+import com.auth.gateway.model.User;
+import com.auth.gateway.responses.LoginResponse;
+import com.auth.gateway.service.AuthenticationService;
+import com.auth.gateway.service.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
 public class AuthenticationController {
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService){
+        this.jwtService = jwtService;
+        this.authenticationService =authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto){
+        User registeredUser = authenticationService.signUp(registerUserDto);
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto){
+        User authenticatedUser = authenticationService.authenticate((loginUserDto));
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+        LoginResponse loginResponse = new LoginResponse(jwtToken, jwtService.getJwtExpirationTime());
+        return ResponseEntity.ok(loginResponse);
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<?> verifyUser(@RequestBody VerifyUserDto verifyUserDto){
+        try{
+            authenticationService.verifiyUser(verifyUserDto);
+            return ResponseEntity.ok("Account verified successfully");
+        }catch(RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/resend")
+    public ResponseEntity<?> resendVerificationCode(@RequestBody String email){
+        try{
+            authenticationService.resentVerificationCode(email);
+            return ResponseEntity.ok("Verification code sent");
+        }catch(RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
 }
---
+++ src/main/java/com/auth/gateway/service/UserService.java
@@ -0,0 +1,5 @@
+package com.auth.gateway.service;
+
+
+public class UserService {
+}
