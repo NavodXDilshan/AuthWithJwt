fccc03eef7666483c3b31277c0323e077f265c12
NavodXDilshan <en97569@foe.sjp.ac.lk>
2026-07-31 00:13:32 +0530

    Authentication part done

--- src/main/java/com/auth/gateway/service/UserService.java
+++ src/main/java/com/auth/gateway/service/UserService.java
@@ -1,5 +1,22 @@
 package com.auth.gateway.service;
 
 
+import com.auth.gateway.model.User;
+import com.auth.gateway.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository, EmailService emailService){
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers(){
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
 }
